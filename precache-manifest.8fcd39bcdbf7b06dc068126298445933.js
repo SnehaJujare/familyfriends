f19b177c2d88e8a0f09850b3f7076b136c11ec74
SnehaJujare <jujaresneha@gmail.com>
2020-05-20 22:50:46 +0530

    Updates

---
+++ precache-manifest.8fcd39bcdbf7b06dc068126298445933.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9212c668d0e43450d972c2b8dda7bea",
+    "url": "/SnehaJujare/familyfriends/index.html"
+  },
+  {
+    "revision": "611915850696d53e9aa0",
+    "url": "/SnehaJujare/familyfriends/static/css/2.7a3f122f.chunk.css"
+  },
+  {
+    "revision": "a0af78f8d800835d8986",
+    "url": "/SnehaJujare/familyfriends/static/css/main.5ef4ee1a.chunk.css"
+  },
+  {
+    "revision": "611915850696d53e9aa0",
+    "url": "/SnehaJujare/familyfriends/static/js/2.9ae2a642.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/SnehaJujare/familyfriends/static/js/2.9ae2a642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0af78f8d800835d8986",
+    "url": "/SnehaJujare/familyfriends/static/js/main.52f2a134.chunk.js"
+  },
+  {
+    "revision": "5a9ca06e73e38bcef0b8",
+    "url": "/SnehaJujare/familyfriends/static/js/runtime-main.c0cd9874.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/SnehaJujare/familyfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
